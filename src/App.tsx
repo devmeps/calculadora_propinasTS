80c789b98bf03a93ed5811ab1aff204ac038bb79
Maximo Eduardo <devmeps@outlook.com>
2024-07-23 23:35:44 -0500

    cambios de nuevo hook y menu item

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { MenuItem } from "./components/MenuItem"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  console.log(menuItems)
-  
+  const { addItem } = useOrder()
 
   return (
     <>
@@ -14,8 +15,18 @@ function App() {
       </header>
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-        <h2>Menu</h2>
+        <div className="p-5">
+        <h2 className="text-4xl font-black">Menu</h2>
+
+        <div className="space-y-3 mt-10">
+        {menuItems.map(item => (
+          <MenuItem
+            key={item.id}
+            item={item}
+            addItem={addItem}
+          />
+        ))}
+        </div>
         </div>
         <div>
         <h2>Consumo</h2>
